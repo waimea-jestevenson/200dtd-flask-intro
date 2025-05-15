61001174683148ed03ffe71d9fa3f60e7dfa4e2f
waimea-jestevenson <jestevenson@waimea.school.nz>
2025-05-15 12:08:57 +1200

    tidying up the website

--- app/__init__.py
+++ app/__init__.py
@@ -5,13 +5,14 @@ from random import randint
 # Create the app 
 app = Flask(__name__)
 
-
+@app.get("/random/")
+def random():
+ randNum = (randint(1,1000))
+ return render_template('pages/random.jinja', number=randNum)
 @app.get("/")
 def home():
   return render_template('pages/home.jinja')
-@app.get("/random/")
-def random():
-    return  str (randint(1,1000))
+
 @app.get("/about/")
 def about():
   return render_template('pages/about.jinja')
